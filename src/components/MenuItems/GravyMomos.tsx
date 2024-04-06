e13ce197ea09f054323a4ca0ade9c34d92999025
0xp0tato <ninadmaster05@gmail.com>
2024-04-06 18:25:18 +0530

    FIX : added key in map

--- src/components/MenuItems/GravyMomos.tsx
+++ src/components/MenuItems/GravyMomos.tsx
@@ -31,8 +31,11 @@ export default function GravyMomos() {
   ];
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-      {items?.map((item: IItems) => (
-        <div className="flex flex-col rounded-lg p-3 shadow-[0_2px_4px_rgba(0,0,0,0.1)] hover:-translate-y-[5px] hover:[transition:transform_0.3s_ease] hover:0_6px_12px_rgba(0,0,0,0.15)">
+      {items?.map((item: IItems, index) => (
+        <div
+          key={index}
+          className="flex flex-col rounded-lg p-3 shadow-[0_2px_4px_rgba(0,0,0,0.1)] hover:-translate-y-[5px] hover:[transition:transform_0.3s_ease] hover:0_6px_12px_rgba(0,0,0,0.15)"
+        >
           <ItemCard item={item} />
         </div>
       ))}
