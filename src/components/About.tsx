edb8aa384e2509bda56259a1934213b97acec642
0xp0tato <ninadmaster05@gmail.com>
2024-08-03 16:15:09 +0530

    muted off

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -45,7 +45,6 @@ const About = () => {
             autoPlay={true}
             loop={true}
             playsInline={true}
-            muted
             className="object-cover rounded-2xl w-full h-full"
           />
         </section>
