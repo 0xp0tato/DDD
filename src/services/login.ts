f336bc8525da3534890437918b7230a0d6008c85
0xp0tato <ninadmaster05@gmail.com>
2024-10-03 12:32:32 +0530

    FIX : password plain text fixed

--- src/services/login.ts
+++ src/services/login.ts
@@ -2,14 +2,26 @@ import connectToDB from "@/lib/dbConnect";
 import { createJWT } from "@/lib/jwt";
 import Users from "@/models/User";
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 
 export async function loginService(username: string, password: string) {
   await connectToDB();
 
   try {
-    const checkUser = await Users.findOne({ username, password });
+    const checkUser = await Users.findOne({ username });
 
     if (!checkUser)
+      return NextResponse.json(
+        { message: "Invalid Username & Password", success: false },
+        { status: 400 }
+      );
+
+    const checkPassword = bcrypt.compare(password, checkUser.password);
+
+    console.log(password);
+    console.log(checkUser.password);
+
+    if (!checkPassword)
       return NextResponse.json(
         { message: "Invalid Username & Password", success: false },
         { status: 400 }
